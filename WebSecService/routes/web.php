dbb48081e20b0db374f5519e8648f315bb528b9c
Kareem Diaa Helal <kareemdiaahelal@gmail.com>
2026-03-16 02:15:45 +0200

    Exercise 1/1: Basic Laravel app with all Chapter 1 examples

--- WebSecService/routes/web.php
+++ WebSecService/routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //welcome.blade.php
+});
+
+Route::get('/multable/{number?}', function ($number = null) {
+    $j = $number ?? 6;
+    return view('multable', compact('j'));
+}); //multable.blade.php
+
+Route::get('/even', function () {
+    return view('even'); //even.blade.php
+});
+
+Route::get('/odd', function () {
+    return view('odd'); //odd.blade.php
+});
+
+Route::get('/prime', function () {
+    return view('prime'); //prime.blade.php
 });
